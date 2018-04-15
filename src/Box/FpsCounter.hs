503da026717ec9892aa9d9478fda71e804fda3ad
Viktor Tanyi <tanyi.viktor@gmail.com>
2018-04-15 13:37:14 +0200

    Initial commit

---
+++ src/Box/FpsCounter.hs
@@ -0,0 +1,37 @@
+module Box.FpsCounter (new, withFps) where
+
+import qualified SDL.Raw
+
+import Data.IORef
+import Data.Word (Word64)
+
+sampledFrameCount :: Int
+sampledFrameCount = 20
+
+data Counter = Counter
+  { _frameCount :: IORef Int
+  , _counterSum :: IORef Word64
+  , _counterFrequency :: !Word64
+  }
+
+new :: IO Counter
+new = do
+  freq <- SDL.Raw.getPerformanceFrequency
+  count <- newIORef 0
+  s <- newIORef 0
+  pure $ Counter count s freq
+
+withFps :: Counter -> (Maybe Float -> IO a) -> IO a
+withFps (Counter countRef sumRef freq) f = do
+  start <- SDL.Raw.getPerformanceCounter
+  count <- readIORef countRef
+  s <- readIORef sumRef
+  let updateNow = (count == sampledFrameCount)
+      fps
+        | updateNow = Just $ 1 / (fromIntegral s / fromIntegral count / fromIntegral freq)
+        | otherwise = Nothing
+  x <- f fps
+  end <- SDL.Raw.getPerformanceCounter
+  writeIORef countRef (if updateNow then 0 else count + 1)
+  writeIORef sumRef (if updateNow then 0 else s + (end - start))
+  pure x
---
+++ src/Main.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import qualified Box.FpsCounter as FpsCounter
+import qualified Data.Text as Text
+import qualified SDL
+
+import Data.Function (fix)
+import SDL (($=))
+import Text.Printf (printf)
+
+main :: IO ()
+main = do
+  SDL.initializeAll
+  window <- SDL.createWindow "" SDL.defaultWindow
+  renderer <- SDL.createRenderer window (-1) $
+    SDL.defaultRenderer { SDL.rendererType = SDL.AcceleratedVSyncRenderer }
+  fpsCounter <- FpsCounter.new
+  fix $ \cont -> do
+    next <- FpsCounter.withFps fpsCounter $ \fps -> do
+      case fps of
+        Just updated -> SDL.windowTitle window $= Text.pack (printf "FPS: %.2f" updated)
+        Nothing -> pure ()
+
+      events <- SDL.pollEvents
+      SDL.present renderer
+      pure $ null [() | SDL.Event { SDL.eventPayload = SDL.QuitEvent } <- events]
+    if next then cont else pure ()
+  SDL.quit
