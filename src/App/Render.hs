ebee6613a79a384dd67259c6473f10fc2abea537
Viktor Tanyi <tanyi.viktor@gmail.com>
2018-04-17 19:53:11 +0200

    Render blocks with outline

--- src/App/Render.hs
+++ src/App/Render.hs
@@ -12,11 +12,13 @@ render :: SDL.Renderer -> GameState.GameState -> IO ()
 render renderer gs = do
   SDL.rendererDrawColor renderer $= V4 0 0 0 255
   SDL.clear renderer
-  SDL.rendererDrawColor renderer $= V4 127 127 127 255
   let camera = view #_camera gs
-  for_ (view #_blockById gs) $ \block ->
+  for_ (view #_blockById gs) $ \block -> do
     let origin = Camera.pointToScreen camera $ view #_origin block
         extent = Camera.vectorToScreen camera $ view #_extent block
         rect = fromIntegral <$> SDL.Rectangle (SDL.P origin) extent
-     in SDL.fillRect renderer $ Just rect
+    SDL.rendererDrawColor renderer $= V4 127 127 127 255
+    SDL.fillRect renderer $ Just rect
+    SDL.rendererDrawColor renderer $= V4 63 63 63 255
+    SDL.drawRect renderer $ Just rect
   SDL.present renderer
